f2e1f15a3807a7aaa13940fc78d68835d08ccad5
Michał Chruściński <m.chruscinski1994@gmail.com>
2023-06-20 22:03:43 +0200

    More experiments

--- buffered_processing.py
+++ buffered_processing.py
@@ -8,7 +8,7 @@ import queue
 
 import numpy as np
 import sounddevice as sd
-# import soundfile as sf
+import soundfile as sf
 import whisper
 
 
@@ -42,35 +42,38 @@ def main():
     print("Done recording.")
     i = 0
     print(f"qsize: {recording_buffer.queue.qsize()}")
-    while recording_buffer.queue.qsize() > 0:
-        try:
-            if i % 50 == 0:
-                print(f"qsize: {recording_buffer.queue.qsize()}")
-            processing_chunk: np.ndarray = recording_buffer.queue.get(block=False)
-            processing_chunk = processing_chunk.reshape((-1,))
-            
-            chunk_copy = processing_chunk.copy()
-            transcribe_buffer = np.append(transcribe_buffer, 
-                                          chunk_copy, 
-                                          axis=0)
-            if len(transcribe_buffer) >= min_frames_to_process:
-                result = model.transcribe(transcribe_buffer, language="pl")
-                # Expand the data if transcription yields nothing
-                if len(result.get("text", "")) > 0:
-                    # If transcription was successful and the buffer is not
-                    # empty - clear it
+    with sf.SoundFile("recording.wav", mode='w', samplerate=samplerate,
+                          channels=channels) as file:
+        while recording_buffer.queue.qsize() > 0:
+            try:
+                if i % 50 == 0:
                     print(f"qsize: {recording_buffer.queue.qsize()}")
-                    print(f"transcribe_buffer length: {len(transcribe_buffer)}")
-                    print(f"result: {result}")
-                    print("Clearing transcribe_buffer")
-                    print("=" * 80)
-                    del transcribe_buffer
-                    transcribe_buffer = np.ndarray(shape=(0,), dtype=np.float32)
-            recording_buffer.queue.task_done()
-            i += 1
-        except queue.Empty:
-            processing = False
-            # pass
+                processing_chunk: np.ndarray = recording_buffer.queue.get(block=False)
+                file.write(processing_chunk)
+                processing_chunk = processing_chunk.reshape((-1,))
+                
+                chunk_copy = processing_chunk.copy()
+                transcribe_buffer = np.append(transcribe_buffer, 
+                                            chunk_copy, 
+                                            axis=0)
+                if len(transcribe_buffer) >= min_frames_to_process:
+                    result = model.transcribe(transcribe_buffer, language="pl")
+                    # Expand the data if transcription yields nothing
+                    if len(result.get("text", "")) > 0:
+                        # If transcription was successful and the buffer is not
+                        # empty - clear it
+                        print(f"qsize: {recording_buffer.queue.qsize()}")
+                        print(f"transcribe_buffer length: {len(transcribe_buffer)}")
+                        print(f"result: {result}")
+                        print("Clearing transcribe_buffer")
+                        print("=" * 80)
+                        del transcribe_buffer
+                        transcribe_buffer = np.ndarray(shape=(0,), dtype=np.float32)
+                recording_buffer.queue.task_done()
+                i += 1
+            except queue.Empty:
+                processing = False
+                # pass
     print("Closing recording stream")
     rec_stream.close()
     return 0
@@ -81,17 +84,13 @@ class RecordingBuffer:
         self.queue = queue.Queue()
         self.total_samples = 0
         self.recording = True
-        self.printed = False
 
     def __call__(self, indata: np.ndarray, frames: int, time: "CData", 
                  status: sd.CallbackFlags):
         if status:
             print(status)
         else:
-            if not self.printed:
-                print(indata.reshape((-1,))[:5])
-                self.printed = True
-            self.queue.put(indata)
+            self.queue.put(indata.copy())
             self.total_samples += len(indata)
     
     def mark_finished(self):
